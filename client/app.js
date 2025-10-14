f564e0ed96aa853e911c48701d45f33aacd3ac20
Lachi9959 <lakshminarayanaburugula@gmail.com>
2025-10-14 12:06:30 +0530

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
 import axios from 'axios';
 import './style.css';
+
 
 const API_BASE = "https://edulink-lms.onrender.com";
 
